3d747611ab19af2f26d26f001a217fc9370b0d53
Bongani Gumede <Mtesazi@gmail.com>
2026-07-06 18:18:08 +0200

    feat: implement API Gateway

---
+++ services/gateway-service/src/main/java/com/mtesazi/gatewayservice/config/GatewayServicesProperties.java
@@ -0,0 +1,32 @@
+package com.mtesazi.gatewayservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "services")
+public class GatewayServicesProperties {
+
+    private final ServiceTarget employee = new ServiceTarget();
+    private final ServiceTarget department = new ServiceTarget();
+
+    public ServiceTarget getEmployee() {
+        return employee;
+    }
+
+    public ServiceTarget getDepartment() {
+        return department;
+    }
+
+    public static class ServiceTarget {
+        private String baseUrl;
+
+        public String getBaseUrl() {
+            return baseUrl;
+        }
+
+        public void setBaseUrl(String baseUrl) {
+            this.baseUrl = baseUrl;
+        }
+    }
+}
