d348137e56ffd6dba2df8d738c570956422f4e74
Bongani Gumede <Mtesazi@gmail.com>
2026-07-07 19:19:50 +0200

    feat(service-discovery): implement Eureka Discovery Server

--- services/department-service/src/test/java/com/mtesazi/departmentservice/DepartmentServiceApplicationTests.java
+++ services/department-service/src/test/java/com/mtesazi/departmentservice/DepartmentServiceApplicationTests.java
@@ -3,7 +3,11 @@ package com.mtesazi.departmentservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "eureka.client.enabled=false",
+        "spring.cloud.discovery.enabled=false",
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
+})
 class DepartmentServiceApplicationTests {
 
 	@Test
