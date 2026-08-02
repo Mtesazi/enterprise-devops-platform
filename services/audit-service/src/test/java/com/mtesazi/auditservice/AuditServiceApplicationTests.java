1f208a2d8f433d698aebae9d8b31cd67e0766484
Bongani Gumede <Mtesazi@gmail.com>
2026-08-02 11:57:33 +0200

    feat(config): create centralized configuration repository

--- services/audit-service/src/test/java/com/mtesazi/auditservice/AuditServiceApplicationTests.java
+++ services/audit-service/src/test/java/com/mtesazi/auditservice/AuditServiceApplicationTests.java
@@ -3,7 +3,15 @@ package com.mtesazi.auditservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.cloud.config.enabled=false",
+        "spring.main.lazy-initialization=true",
+        "eureka.client.enabled=false",
+        "spring.cloud.discovery.enabled=false",
+        "spring.kafka.listener.auto-startup=false",
+        "spring.kafka.bootstrap-servers=localhost:9092",
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
+})
 class AuditServiceApplicationTests {
 
 	@Test
--- services/employee-service/src/test/java/com/mtesazi/employeeservice/EmployeeServiceApplicationTests.java
+++ services/employee-service/src/test/java/com/mtesazi/employeeservice/EmployeeServiceApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest(properties = {
         "spring.cloud.config.enabled=false",
+        "spring.main.lazy-initialization=true",
         "eureka.client.enabled=false",
         "spring.cloud.discovery.enabled=false",
         "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration,org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
