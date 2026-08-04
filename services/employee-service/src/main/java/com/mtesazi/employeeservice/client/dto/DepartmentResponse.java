178c95dca92c1b7735ac7a1fe8fd31c3318874b2
Bongani Gumede <Mtesazi@gmail.com>
2026-08-04 18:28:07 +0200

    Dockerfiles and Docker Compose

--- services/employee-service/src/main/java/com/mtesazi/employeeservice/client/dto/DepartmentResponse.java
+++ services/employee-service/src/main/java/com/mtesazi/employeeservice/client/dto/DepartmentResponse.java
@@ -3,6 +3,7 @@ package com.mtesazi.employeeservice.client.dto;
 public record DepartmentResponse(
         Long id,
         String name,
-        String code
+        String code,
+        String description
 ) {
 }
